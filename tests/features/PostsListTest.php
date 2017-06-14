10d14ae8cddb2a7e084dce62d7ed398c12b2b82d
jgutzm <jgutzm@gmail.com>
2017-06-14 11:40:05 +0200

    New test_the_posts_are_paginated TDD feature

--- tests/features/PostsListTest.php
+++ tests/features/PostsListTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Post;
+use Carbon\Carbon;
+
 class PostsListTest extends FeatureTestCase
 {
     function test_a_user_can_see_the_posts_list_and_go_to_the_details()
@@ -17,5 +20,34 @@ class PostsListTest extends FeatureTestCase
             ->see($post->title)
             ->click($post->title)
             ->seePageIs($post->url);
+    }
+
+    function test_the_posts_are_paginated()
+    {
+        // Having
+        $oldest = factory(Post::class)->create([
+            'title' => 'Post más antiguo',
+            'created_at' => Carbon::now()->subDays(2)
+        ]);
+
+        factory(Post::class)->times(15)->create([
+            'created_at' => Carbon::now()->subDay()
+        ]);
+
+        $newest = factory(Post::class)->create([
+            'title' => 'Post más reciente',
+            'created_at' => Carbon::now()
+        ]);
+
+        // When
+        $this->visit('/');
+
+        // Then
+        $this->see($newest->title)
+            ->dontSee($oldest->title)
+            ->click('2')
+            ->see($oldest->title)
+            ->dontSee($newest->title);
+
     }
 }
