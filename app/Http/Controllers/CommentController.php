04a10f186fc1c78a484c7560e835b123a8534b5d
jgutzm <jgutzm@gmail.com>
2017-06-15 10:54:23 +0200

    New test_create_comment_form_validation TDD feature

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,6 +10,10 @@ class CommentController extends Controller
 {
     public function store(Request $request, Post $post)
     {
+        $this->validate($request, [
+          'comment' => 'required'
+        ]);
+
         auth()->user()->comment($post, $request->get('comment'));
 
         return redirect($post->url);
